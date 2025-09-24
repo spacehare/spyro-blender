59fd79892649ce43f021f01c4aa5dd47e115bb44
spacehare <dantath425@gmail.com>
2025-09-23 22:58:46 -0400

    fix setup order

--- spyroboxes/do_everything.py
+++ spyroboxes/do_everything.py
@@ -6,9 +6,9 @@ from . import setup
 def do_everything(*, should_setup: bool = False, mesh_folder_path: str = '', render_path: str = '', render_xy: int = 0):
     if should_setup:
         setup.set_render_file_format()
-        setup.setup_compositor()
         setup.setup_render_settings()
         setup.setup_viewlayers_and_collections()
+        setup.setup_compositor()
         setup.setup_camera()
 
     if mesh_folder_path:
